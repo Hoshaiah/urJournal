c3a2a726a3bce4457d8b7cf87d8a9592c64b54ab
Hoshaiah Domingo <domingohoshaiah@gmail.com>
2022-06-28 17:51:10 +0800

    working code for firebase, android on windows

--- App.js
+++ App.js
@@ -5,6 +5,23 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import LogsScreen from './screens/LogsScreen';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0Y4kSUomRbQG2sl7Qfk0JIhATXHJwcpQ",
+  authDomain: "urjournal-b81d2.firebaseapp.com",
+  projectId: "urjournal-b81d2",
+  storageBucket: "urjournal-b81d2.appspot.com",
+  messagingSenderId: "570590754389",
+  appId: "1:570590754389:web:d70dfd3b8c0f015493c6dc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const Stack = createNativeStackNavigator()
 
 export default function App() {

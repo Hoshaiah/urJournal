50ad36dbce04f7812f44ca30a701186b9cbf547d
Hosh <domingohoshaiah@gmail.com>
2022-07-16 20:13:41 +0800

    firebase authentication now working

--- App.js
+++ App.js
@@ -5,12 +5,11 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import LogsScreen from './screens/LogsScreen';
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB0Y4kSUomRbQG2sl7Qfk0JIhATXHJwcpQ",
   authDomain: "urjournal-b81d2.firebaseapp.com",
@@ -19,9 +18,18 @@ const firebaseConfig = {
   messagingSenderId: "570590754389",
   appId: "1:570590754389:web:d70dfd3b8c0f015493c6dc"
 };
+export const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+function setupHighscoreListener(userId) {
+  const db = getDatabase();
+  const reference = ref(db, 'users/' + userId);
+  onValue(reference, (snapshot) => {
+    const highscore = snapshot.val().highscore;
+    console.log("New high score: " + highscore);
+  });
+}
+
+
 const Stack = createNativeStackNavigator()
 
 export default function App() {
--- screens/LogsScreen.js
+++ screens/LogsScreen.js
@@ -1,22 +1,45 @@
 import { TouchableOpacity, StyleSheet, Text, View, KeyboardAvoidingView, TextInput } from 'react-native';
 import { useState } from 'react'
 // import { auth } from '../firebase';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth';
+import { app } from '../App.js'
 
 function LogsScreen () {
     const [email, setEmail]= useState('')
     const [password, setPassword]= useState('')
-    
+    const auth = getAuth(app);
+    const [user, setUser] = useState(null)
+
     const handleSignUp = () => {
-    //     // auth
-    //     .createUserWithEmailAndPassword(email, password)
-    //     .then(userCredentials => {
-    //         const user = userCredentials.user;
-    //         console.log(user.email)
-    //     }).catch(error => {
-    //         alert(error.message)
-    //         console.log(error)
-    //     })
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            setUser(userCredential.user);
+            console.log(`sign up successful with ${user.email}, user logged in`)
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorMessage)
+        });
     }
+    const handleLogin = () => {
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            setUser(userCredential.user);
+            console.log(`log in successful with ${user.email}`)
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(error.message)
+        });
+    }
+    const logUserCredentials = () => {
+        console.log(user)
+    }
+
     return (
         <KeyboardAvoidingView style={styles.container}>
             <Text>Logs Screen</Text>
@@ -29,13 +52,18 @@ function LogsScreen () {
             <View>
                 <TouchableOpacity 
                     style={styles.button}
-                    onPress={()=> {console.log(email)}}>
+                    onPress={handleLogin}>
                     <Text>Login</Text>
                 </TouchableOpacity>
                 <TouchableOpacity 
                     style={styles.button}
                     onPress={handleSignUp}>
                     <Text>Signup</Text>
+                </TouchableOpacity>
+                <TouchableOpacity 
+                    style={styles.button}
+                    onPress={logUserCredentials}>
+                    <Text>LogUserCredentials</Text>
                 </TouchableOpacity>
             </View>
         </KeyboardAvoidingView>
